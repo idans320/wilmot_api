11d9dc4b93031b56afc9fdebf6490b6822366b10
Idan Shani <idans@stltd.co.il>
2021-05-30 11:56:52 +0300

    login form

--- routes/login.js
+++ routes/login.js
@@ -1,43 +1,59 @@
-import {Router} from "express"
+import { Router } from "express"
 import express from "express"
 import db from "../shared/db.js"
 import $ from "jquery"
 import USER_MODEL from "../schema/user_model.js"
 import Ajv from "ajv";
+import { signToken } from "../shared/jws.js"
 
 const route = Router();
 const ajv = new Ajv()
 
 route.use(express.json())
 
-function submitLogin(e){
+async function submitLogin(e) {
     e.preventDefault();
     let unindexed_array = $(e.target).serializeArray();
     let indexed_array = {};
 
-    $.map(unindexed_array, function(n, i){
+    $.map(unindexed_array, function (n, i) {
         indexed_array[n['name']] = n['value'];
     });
-    console.log(indexed_array)
+    const result = await fetch("/login/", {
+        method: "post", body: JSON.stringify(indexed_array), headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    if (result.status == 200) {
+        const json = await result.json()
+        console.log(json)
+    }
+    else {
+        console.error("Invalid login")
+    }
     return false
 }
 
-route.get("/", (req,res) => {
+route.get("/", (req, res) => {
     console.log(submitLogin)
-    res.render("login", {submit:submitLogin})
+    res.render("login", { submit: submitLogin })
 })
 
-route.post("/", async (req,res) => {
+route.post("/", async (req, res) => {
     const validate = ajv.compile(USER_MODEL);
-    if (!validate(req.body)){
-        return res.sendStatus(409)
+    if (!validate(req.body)) {
+        return res.sendStatus(400)
     }
-    const {username, password} = req.body
-    try{
-        const token = await db.users.generateToken(username,password)
-        res.send(token)
-    } catch(e){
+    const { username, password } = req.body
+    const validLogin = await db.users.login(username, password)
+    if (validLogin) {
+        const token = await signToken(username)
+        res.send({ "token": token })
+    }
+    else {
         res.sendStatus(401)
     }
+
+
 })
 export default route
---
+++ shared/jws.js
@@ -0,0 +1,20 @@
+import FileProvider from 'node-jws-file-provider';
+import {default as JWS, JWTAlghoritm } from "node-jws"
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from "path"
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const provider = FileProvider.default(path.join(__dirname,'../private.pem'), path.join(__dirname,'../public.pem'));
+
+
+export const signToken = async (user) => {
+    const token = new JWS.default(provider);
+    token.useAlghoritm(JWTAlghoritm.RS256);
+    token.setClaims({
+        user: user,
+    });
+    await token.sign()
+    return token.toString()
+}
