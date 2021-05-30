b3b4e0f0c9a42c0efb1b6639d1e8ff7121786995
Idan Shani <idans@stltd.co.il>
2021-05-30 16:07:05 +0300

    add item

--- scripts/add_user.js
+++ scripts/add_user.js
@@ -8,11 +8,11 @@ assert.strictEqual((args.length == 2),true,"USAGE: [USER] [PASSWORD]")
 const [user,password] = args
 
 db.users.register(user,password).then(()=>{ 
-    console.log("User added successfuly")
+    
     process.exit()
 
 },(e) => {
-    console.log(e)
+    
     process.exit()
 })
 
