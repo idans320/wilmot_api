04d94894380e0647650916b03344e8890eb195c6
Idan Shani <idans@stltd.co.il>
2021-05-31 15:24:06 +0300

    delete_item

--- routes/services.js
+++ routes/services.js
@@ -1,5 +1,5 @@
 import Ajv from "ajv";
-import {Router} from "express"
+import { Router } from "express"
 import DATA_SERVICE_SCHEMA from "../schema/data_service.js"
 import db from "../shared/db.js"
 import express from "express"
@@ -11,30 +11,32 @@ const ajv = new Ajv()
 
 let route = Router();
 
-const getServiceFromRoute = async (route) => { 
+const getServiceFromRoute = async (route) => {
     let dir = path.dirname(route)
     let name = path.basename(route)
-    
-    let service = await db.services.getServiceByRouteAndName(dir,name)
-    
+    dir = dir.endsWith("/") ? dir : dir + "/"
+
+    let service = await db.services.getServiceByRouteAndName(dir, name)
+
     return service
 }
 
-const sendAuthorization = async function sendAuthorization(path, redirect){ 
+const sendAuthorization = async function sendAuthorization(path, redirect) {
     const token = window.localStorage.getItem("token")
-    if (token){
-        const request = await (await fetch(path,{headers: {authorization:token}})).text()
+    if (token) {
+        const request = await (await fetch(path, { headers: { authorization: token } })).text()
         document.open()
         document.write(request)
         document.close()
     }
-    else if (redirect){
+    else if (redirect) {
         window.location.href = "/login/";
     }
 }
 
-const submitData = async function submitData(e){
+const submitData = async function submitData(e, method) {
     e.preventDefault();
+    debugger
     const token = window.localStorage.getItem("token")
 
     let unindexed_array = $(e.target).serializeArray();
@@ -43,16 +45,16 @@ const submitData = async function submitData(e){
     $.map(unindexed_array, function (n, i) {
         indexed_array[n['name']] = n['value'];
     });
-    const url = window.location.pathname.replace("/add_item","")
-    indexed_array["data"] = JSON.parse(indexed_array["data"])
+    const url = window.location.pathname + "/../"
+    indexed_array["data"] = indexed_array["data"] ? JSON.parse(indexed_array["data"]) : {}
     const result = await fetch(url, {
-        method: "post", body: JSON.stringify(indexed_array), headers: {
+        method: method, body: JSON.stringify(indexed_array), headers: {
             'Content-Type': 'application/json',
-            "authorization":token
+            "authorization": token
         }
     })
     if (result.status == 201) {
-        window.location.href = url
+        window.location.href = "../"
     }
     else {
         alert("Invalid Request")
@@ -65,51 +67,86 @@ route.use(express.json())
 route.get('/:path*?/add_item', async function (req, res) {
     const token = req.headers.authorization
     const isValid = token ? await verify(token) : false
-    
-    res.render("add_item",{isValid:isValid, tokenSent: Boolean(token),sendAuthorization:sendAuthorization, submitData:submitData})
+
+    res.render("add_item", { isValid: isValid, tokenSent: Boolean(token), sendAuthorization: sendAuthorization, submitData: submitData })
 })
+
+route.get('/:path*?/delete_item', async function (req, res) {
+    const route = getRoute(req)
+        .replace("/delete_item", "")
+    const token = req.headers.authorization
+    const isValid = token ? await verify(token) : false
+    let services = await db.services.getServicesInRoute(route)
+    res.render("delete_item", { isValid: isValid, services: services, tokenSent: Boolean(token), sendAuthorization: sendAuthorization, submitData: submitData })
+})
+
+const getRoute = (req) => {    
+    let route = req.params["path"] ? ("/" + (req.params["path"]) + (req.params[0] ? req.params[0] : "")) : "/"
+    return route.endsWith("/") ? route : route + "/"
+}
 
 route.get('/:path*?', async function (req, res) {
     const token = req.headers.authorization
     const isValid = token ? await verify(token) : false
 
-    const route = req.params["path"] ? ("/" + (req.params["path"]) + (req.params[0] ? req.params[0] : "")) : "/"
+    const route = getRoute(req)
     let service = await getServiceFromRoute(route)
     let routes = await db.services.getChildrenRoutes(route)
-    if (service){
+    if (service) {
         return res.send(service.data)
     }
-    else{
-        if (!req.originalUrl.endsWith("/")){
+    else {
+        if (!req.originalUrl.endsWith("/")) {
             return res.redirect(req.originalUrl + "/")
         }
         let services = await db.services.getServicesInRoute(route)
-        res.render('services', {services: services, routes : routes, path:route, isValid:isValid, tokenSent: Boolean(token),sendAuthorization:sendAuthorization})
+        res.render('services', { services: services, routes: routes, path: route, isValid: isValid, tokenSent: Boolean(token), sendAuthorization: sendAuthorization })
     }
 })
 
-route.post("/:path?*", async function (req, res){
+route.post("/:path?*", async function (req, res) {
     const token = req.headers.authorization
     const isValid = token ? await verify(token) : false
-    if (isValid){
-        const route = req.params["path"] ? ("/" + (req.params["path"]) + (req.params[0] ? req.params[0] : "")) : "/"
+    if (isValid) {
+        const route = getRoute(req)
         const data = req.body
         let service = await getServiceFromRoute(route)
-        if (service){return res.sendStatus(409) }
+        if (service) { return res.sendStatus(409) }
         data.route = route
         const validate = ajv.compile(DATA_SERVICE_SCHEMA)
-        if (validate(data)){
-            const service = await db.services.getServiceByRouteAndName(route,data.name)
-            if (service){ res.sendStatus(409)}
-            else{
+        if (validate(data)) {
+            const service = await db.services.getServiceByRouteAndName(route, data.name)
+            if (service) { res.sendStatus(409) }
+            else {
                 res.sendStatus(201)
                 db.services.addService(data)
             }
         } else {
             res.sendStatus(400)
         }
-    } else{
+    } else {
         res.sendStatus(401)
+    }
+})
+
+route.delete("/:path?*", async function (req, res) {
+    const token = req.headers.authorization
+    const isValid = token ? await verify(token) : false
+    if (isValid) {
+        const route = getRoute(req)
+        const data = req.body
+        if (data.name) {
+            let service = await db.services.getServiceByRouteAndName(route, data.name)
+            if (service) {
+                data.route = route
+                res.sendStatus(201)
+                db.services.deleteService(data)
+            } else {
+                res.sendStatus(404)
+            }
+        } else {
+            res.sendStatus(400)
+        }
     }
 })
 
